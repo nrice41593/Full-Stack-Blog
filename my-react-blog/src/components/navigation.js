2e3afbca162d23133c266860c33e4cbc45f41e1d
Nathan Rice <nrice778@gmail.com>
2021-01-04 14:19:45 -0600

    init commit

---
+++ my-react-blog/src/components/navigation.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import { Avatar } from 'antd';
+
+const navLinks = [
+    {
+      title: 'Blog',
+      path: '/blog'
+    },
+    {
+      title: 'Web Dev',
+      path: '/web-dev'
+    },
+    {
+      title: 'Cloud',
+      path: '/cloud'
+    },
+    {
+      title: 'Algorithms',
+      path: '/algos'
+    },
+    {
+      title: 'Brain Hacking',
+      path: '/health'
+    },
+    {
+      title: 'Login',
+      path: '/login'
+    }
+  ]
+
+export default function Navigation () {
+    const [menuActive, setMenuActive] = useState(false)
+
+    return (
+    <nav className={`site-navigation ${menuActive && 'active'}`} role="navigation">
+        <span className="menu-title"><a href="/home">The Chunky Salsa</a></span>
+        <div
+            className="menu-content-container"
+        >
+            <ul>
+            { navLinks.map((link, index) => (
+                <li key={index}>
+                    <Link to={link.path}>{link.title}</Link>
+                </li>
+                ))
+            }
+            </ul>
+            <div className="menu-avatar-container">
+                <Avatar size={50} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                <span className="menu-avatar-name">Nathan Rice</span>
+            </div>
+        </div>
+        <i 
+            className="icon ionicons ion-ios-menu"
+            onClick={(ev) => setMenuActive(!menuActive)}
+        />
+    </nav>
+  )
+}
